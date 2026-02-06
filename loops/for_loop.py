a221d3a66feda2cde52edd3d85ac88c361f1a2d1
Nurdaulet <nurdauletajtbay@gmail.com>
2026-02-06 05:01:46 +0500

    Initial commit

---
+++ loops/for_loop.py
@@ -0,0 +1,20 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+for x in "banana":
+    print(x)
+
+for x in range(5):
+    print(x)
+
+for x in range(1, 5):
+    print(x)
+
+for x in range(1, 5, 3):
+    print(x)
+
+for x in range(5):
+    print(x)
+else:
+    print("Finally finished!")
